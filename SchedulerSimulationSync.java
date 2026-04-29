bf25dd8068142cbd05668d4e157ca4674e7ab0df
rashid-sattam-444 <444050355@std.psau.edu.sa>
2026-04-29 22:06:12 +0300

    Add a Semaphore to limit concurrent process execution

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -31,6 +31,7 @@ class SharedResources {
     public static final ReentrantLock completedProcessLock = new ReentrantLock();
     public static final ReentrantLock waitingTimeLock = new ReentrantLock();
     public static final ReentrantLock logLock = new ReentrantLock();
+    public static final Semaphore cpuSemaphore = new Semaphore(1);
 
     public static int contextSwitchCount = 0; // Shared counter - NEEDS PROTECTION!
     public static int completedProcessCount = 0; // Shared counter - NEEDS PROTECTION!
