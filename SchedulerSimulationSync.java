0b744e879b7694b463dde088668f8e9eb367d721
rashid-sattam-444 <444050355@std.psau.edu.sa>
2026-04-30 23:06:25 +0300

    implement safe CPU resource management using Semaphore acquire and release

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -197,22 +197,28 @@ class Process implements Runnable {
     }
 
     public void runToCompletion() {
-        // TODO: Similar synchronization needed here
         try {
-            System.out.println(Colors.BRIGHT_CYAN + "  ⚡ " + Colors.BOLD + Colors.CYAN + name +
-                    Colors.RESET + Colors.BRIGHT_CYAN + " is the last process, running to completion" +
-                    Colors.RESET + " [" + remainingTime + "ms]");
-            Thread.sleep(remainingTime);
-            remainingTime = 0;
-            completionTime = System.currentTimeMillis();
+            SharedResources.cpuSemaphore.acquire();
+            try {
+                System.out.println(Colors.BRIGHT_CYAN + "  ⚡ " + Colors.BOLD + Colors.CYAN + name +
+                        Colors.RESET + Colors.BRIGHT_CYAN + " is the last process, running to completion" +
+                        Colors.RESET + " [" + remainingTime + "ms]");
+                Thread.sleep(remainingTime);
+                remainingTime = 0;
+                completionTime = System.currentTimeMillis();
 
-            long waitingTime = (completionTime - creationTime) - burstTime;
-            SharedResources.addWaitingTime(waitingTime);
-            SharedResources.incrementCompletedProcess();
+                long waitingTime = (completionTime - creationTime) - burstTime;
+                SharedResources.addWaitingTime(waitingTime);
+                SharedResources.incrementCompletedProcess();
 
-            System.out.println(Colors.BRIGHT_GREEN + "  ✓ " + Colors.BOLD + Colors.CYAN + name +
-                    Colors.RESET + Colors.BRIGHT_GREEN + " finished execution!" + Colors.RESET);
-            System.out.println();
+                System.out.println(Colors.BRIGHT_GREEN + "  ✓ " + Colors.BOLD + Colors.CYAN + name +
+                        Colors.RESET + Colors.BRIGHT_GREEN + " finished execution!" + Colors.RESET);
+                System.out.println();
+            }
+
+            finally {
+                SharedResources.cpuSemaphore.release();
+            }
         } catch (InterruptedException e) {
             System.out.println(Colors.RED + "  ✗ " + name + " was interrupted." + Colors.RESET);
         }
